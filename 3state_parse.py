05a36c60721289b8d5dcba3bd39c0b658cb688d7
semccomas <semccomas@gmail.com>
2016-06-29 11:05:57 +0200

    parser for comparing dssp and protein sequence, calculates the difference

--- 3state_parse.py
+++ 3state_parse.py
@@ -1,6 +1,7 @@
 ### this will take the .fa file and the dssp file and compare the two
 
 import sys
+from difflib import *
 
 fa= open(sys.argv[1]).read().splitlines()
 dssp=open(sys.argv[2]).read().splitlines()
@@ -8,13 +9,11 @@ dssp=open(sys.argv[2]).read().splitlines()
 aminodic= {'A':1, 'R':2, 'N':3, 'D':4, 'C':5, 'Q':6, 'E':7, 'G':8, 'H':9, 'I':10, 'L':11, 'K':12, 'M':13, 'F':14, 'P':15, 'S':16, 'T':17, 'W':18, 'Y':19, 'V':20} 
 statedic={'H':0, 'I':0 , 'G':0, 'E':1, 'B':1, 'T':2, 'S':2, 'L':2}
 #0 is helix, 1 is strand, 2 is coil
-amino= []
 
-
+#################################################### parse the DSSP output and find the states and the amino acid ###############################################            
+dsspAA= []
+states= []
 start=0
-
-
-
 for line in dssp:
     value= line.split()
     if 'N-H-->O' in value:                       #indicates where to start parsing
@@ -22,22 +21,54 @@ for line in dssp:
         continue                              #skip the header line
     if not start: 
         continue                              #skip all lines before the header line also
-#### accessing the amino acids only #####
-    if len(value[1])==4:
-        amino.append(value[2])
-        
+
+
+
+    if len(value[1])==4 and value[2] in aminodic:
+        dsspAA.append(value[2])
+        if value[3] in statedic:
+            states.append(value[3])
+        elif value[3] not in statedic:
+            states.append('!!') #### same as below, no state, how to encode??
+    elif len(value[1])!=4 and value[3] in aminodic:
+        dsspAA.append(value[3])
+        if value[4] in statedic:
+            states.append(value[4])
+        elif value[4] not in statedic:
+            states.append('!!') ### this is if there is no state but there is an aa. what to do to be encoded 000??
+
+
+
+
+
+
+##### just for comparing in writing the program #####
+fAA= fa[1]                                         #
+print len(fAA), 'len faa', fAA                     #
+print 'dssp', dsspAA, len(dsspAA)                  #
+print states, len(states), 'states'                #
+                                                   #                                #
+####################################################
+
+
+
+###################################################### comparing the two files, the actual protein (fa) and the dssp file ###################
+
+d=Differ()
+diff= d.compare(fAA, dsspAA)
+comp= '\n'.join(diff)
+
+
+match= 0                                       #need this here as well as enumerate because we only want to count matches
+for n, line in enumerate(comp.splitlines()):
+    if '-' not in line:
+        print line[2], dsspAA[match], states[match]
+        match= match +1 
     else:
-        amino.append(value[3])
+        print fAA[n], '!!', '!!'
+print 'line[2], dsspAA[match], states[match]'
 
-if len(amino) != len(fa[1]):
-    print 'Warning!! Actual protein sequence != PDB prediction!!', len(fa[1]), len(amino)
-else:
-    print 'Actual protein sequence == PDB prediction'
+#here !! symbolizes blank or missing, for both the amino acid and the states
 
-count=0
-for char, a in zip(fa[1], amino):
-	count= count+1
-	if char != a: 
-		print 'missing or unmatching character: %s != %s' % char % a 
-	if char== a:
-		print char
+######################################## one hot encoding the states #####################################
+
