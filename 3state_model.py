bb228563cf0f975e4fdde479a9f097f94372cb3c
semccomas <semccomas@gmail.com>
2016-07-03 11:08:06 +0200

    updates to keras model including starting to parse pytable and plotting results

--- 3state_model.py
+++ 3state_model.py
@@ -4,19 +4,27 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.callbacks import TensorBoard
 import numpy as np
-
+import tables as tb
 
 np.random.seed(3)
-
-dataset = np.loadtxt("dssp_values", delimiter="\t")
-X_train = dataset[:,0]
-Y_train = dataset[:,1:]
-testset=np.loadtxt('dssp_validation', delimiter='\t')
-X_test = testset[:,0]
-Y_test = testset[:,1:]
+#### loading the tables ###
+dataset = tb.open_file('big_table')
+table= dataset.root.group_3EZMA.one_hot
+print table.attrs._f_list('all')
+   
 
 
 
+#X_train = dataset[:,0]
+#Y_train = dataset[:,1:]
+#testset=np.loadtxt('big_test_table', delimiter='\t')
+#X_test = testset[:,0]
+#Y_test = testset[:,1:]
+
+
+
+
+'''
 model = Sequential()
 model.add(Dense(64, input_dim=1, init='uniform', activation='tanh'))
 #model.add(Dropout(0.5))
@@ -24,19 +32,25 @@ model.add(Dense(64, activation='tanh'))
 #model.add(Dropout(0.5))
 model.add(Dense(3, activation='softmax'))
 
+
+
 model.compile(loss='categorical_crossentropy',
               optimizer='adagrad',
               metrics=['accuracy'])
 
+
+
+
 ## sparse categorical was raising errors so I changed to just categorical
 
-batchsize= 100         #using batch size more than once so defining it as a var to not forget to change all values each time
+batchsize= 1000         #using batch size more than once so defining it as a var to not forget to change all values each time
 
-
+nb_epoch= 10
 #TB= TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True)
-history = model.fit(X_train, Y_train, nb_epoch=100, batch_size=batchsize, validation_data=(X_test, Y_test))
+history = model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batchsize, validation_data=(X_test, Y_test))
 
 print history.history
+loss=history.history['loss']
 #this will get us plottable numbers for making a graph. We have to include validation data into the history so that we actually have a testing set in there... 
 
 scores = model.evaluate(X_test, Y_test, batch_size=batchsize)
@@ -45,3 +59,19 @@ print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 #from keras.utils.visualize_util import plot
 #plot(model, to_file='model.png')
+
+
+import matplotlib.pyplot as plt
+
+x= []
+for r in xrange(nb_epoch):
+    x.append(r)
+print x
+
+plt.plot(x, loss)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.show()
+plt.savefig('old_dssp_loss.png')
+
+'''
