5cc4e83cb5208d0bd248df92fc46ae0e00995a09
semccomas <semccomas@gmail.com>
2016-08-26 16:40:36 +0200

    preparing for leaving

--- auto_parse_all_bigdataset.sh
+++ auto_parse_all_bigdataset.sh
@@ -5,33 +5,32 @@ then
 
 	for i in /data/pdbcull/matching_proteins/train_set/*.fa; do  
 	basei=`basename $i` 
-	for j in /data/pdbfiles/train_data/*.pdb; do ## <<< change this to pdb files when you get the chance 
+	for j in /data/pdbfiles/train_data/*.pdb; do 
 	basej=`basename $j` 
 	newi=${basei:0:5} 
 	newj=${basej:0:5}
 	if [ $newi = $newj ] ; then
 	
 	echo $i $j 
-	python parse_all_bigdataset.py $i $j /data/pdbcull/$newi.jhE0.aln big_table_all   ## <<<< note!!! -aln and .fasta files in the pdbcull are kinda different. aln has alignments only and fasta has both alignments and the prot. names of what aligned to 
+	python parse_all_bigdataset.py $i $j /data/pdbcull/$newi.jhE0.aln big_table_all 
 
 	fi
 	done
 	done
 
 
-### NOTE!!! I also currently dont have a validation set.... maybe take like 50 of the proteins from matching_proteins?? 
 	rm big_test_table_all
 
 	for i in /data/pdbcull/matching_proteins/test_set/*.fa; do  
 	basei=`basename $i` 
-	for j in /data/pdbfiles/train_data/*.pdb; do ## <<< change this to pdb files when you get the chance 
+	for j in /data/pdbfiles/train_data/*.pdb; do 
 	basej=`basename $j` 
 	newi=${basei:0:5} 
 	newj=${basej:0:5}
 	if [ $newi = $newj ] ; then
 	
 	echo $i $j 
-	python parse_all_bigdataset.py $i $j /data/pdbcull/$newi.jhE0.aln big_table_all   ## <<<< note!!! -aln and .fasta files in the pdbcull are kinda different. aln has alignments only and fasta has both alignments and the prot. names of what aligned to 
+	python parse_all_bigdataset.py $i $j /data/pdbcull/$newi.jhE0.aln big_test_table_all  
 
 	fi
 	done
@@ -39,5 +38,5 @@ then
 
 
 else
-    echo 'cool'
+    echo 'Ok!'
 fi
